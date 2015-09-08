fe54760c4ff09e7535b6af6401b4c5c838d663ab
Takafumi Iju <takafumi.iju@sk.com>
2015-09-08 13:50:34 +0900

    move

--- SpringBootOnScala/project/plugins.sbt
+++
@@ -1,3 +0,0 @@
-logLevel := Level.Warn
-
-//addSbtPlugin("com.typesafe.sbt" % "sbt-pom-reader" % "2.0.0")
---
+++ SpringPassbook/src/main/java/jp/ijufumi/sample/spring/boot/SpringDemoPassbookApplication.java
@@ -0,0 +1,12 @@
+package jp.ijufumi.sample.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoPassbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoPassbookApplication.class, args);
+    }
+}
